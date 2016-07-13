dcdc52ea2d0f4a18cfc6b037528e696a8287d70a
JRendonASC <jrendon17@cardinalhayes.org>
2016-07-13 09:17:31 -0400

    F

---
+++ Square.py
@@ -0,0 +1,13 @@
+from Myro import *
+init ("sim")
+penDown()
+i=0 
+j=0
+while (i<4):
+        forward (1,2)
+        turnBy (90)
+        i=i+1
+while (j<4):
+        forward (1,2)
+        turnBy (90)
+        j=j+1
---
+++ Triangle.py
@@ -0,0 +1,6 @@
+from Myro import *
+forward (1,1)
+turnBy (120)
+forward (1,1)
+turnBy (115)
+forward (1,1)
