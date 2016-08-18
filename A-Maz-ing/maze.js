f0331c803560a464513cbdabbdd3674cdd1bcaec
JRendonASC <jrendon17@cardinalhayes.org>
2016-08-18 11:12:13 -0400

    Maze

--- A-Maz-ing/maze.js
+++ A-Maz-ing/maze.js
@@ -2,16 +2,17 @@ var x=50;
 var y=50;
 function setup(){
 	createCanvas(600,600);
+	}
+
+function draw(){
 	background(0);
+	fill(255);
 	rect(0,100,450,50);
 	rect(400,200,200,50);
 	rect(0,300,450,50);
 	rect(400,400,200,50);
 	fill(255,0,0);
 	rect(500,500,75,75);
-}
-
-function draw(){
 	if (keyIsDown(LEFT_ARROW))
 		x-=5;
 	if (keyIsDown(RIGHT_ARROW))
@@ -21,5 +22,26 @@ function draw(){
 	if (keyIsDown(DOWN_ARROW))
 		y+=5;
 	fill(0,0,255);
-	ellipse(x,y,40,40);
+	ellipse(x-20,y-20,40,40);
+	if ((x>=0 && x<=450) && (y>=100 && y<=150)){
+	x=50;
+	y=50;
+	ellipse(x-20,y-20,40,40);
+	}
+	if ((x>=400 && x<=600) && (y>=200 && y<=250)){
+	x=50;
+	y=50;
+	ellipse(x-20,y-20,40,40);
+	}
+	if ((x>=0 && x<=450) && (y>=300 && y<=350)){
+	x=50;
+	y=50;
+	ellipse(x-20,y-20,40,40);
+	}
+	if ((x>=400 && x<=600) && (y>=400 && y<=450)){
+	x=50;
+	y=50;
+	ellipse(x-20,y-20,40,40);	
+	}
+	
 }
